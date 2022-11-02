3661a7b0ebc95515420052a272c0a425f5b804ce
HussainMojahid <mojahidhussain2001@gmail.com>
2022-11-02 11:13:10 +0530

    Added allow all origins

--- src/main/java/com/testrig/simulator/controller/CSTServicesController.java
+++ src/main/java/com/testrig/simulator/controller/CSTServicesController.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -24,6 +25,7 @@ import com.testrig.simulator.repo.HttpRoundTripRepo;
 import com.testrig.simulator.service.HttpRoundTripService;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/CSTServices")
 public class CSTServicesController {
 
--- src/main/java/com/testrig/simulator/controller/HttpRoundTripController.java
+++ src/main/java/com/testrig/simulator/controller/HttpRoundTripController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import com.testrig.simulator.repo.HttpRoundTripRepo;
 import com.testrig.simulator.service.HttpRoundTripService;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping(value = "/api")
 public class HttpRoundTripController {
 
